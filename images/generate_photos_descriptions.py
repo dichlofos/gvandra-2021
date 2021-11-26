d62ee4af9c3f64fa1ddd356a21226abc3f6960d4
Mikhail Veltishchev <mvel@yandex-team.ru>
2021-11-26 09:50:58 +0300

    Dump photos data

--- images/generate_photos_descriptions.py
+++ images/generate_photos_descriptions.py
@@ -2,6 +2,7 @@
 
 import re
 import sys
+import json
 
 
 def _flush_block(day, photo_block, report_text):
@@ -12,20 +13,89 @@ def _flush_block(day, photo_block, report_text):
     return re.sub(marker, begin + '\n' + photo_block + end, report_text, flags=re.DOTALL)
 
 
-_REPORT_NAME = "../report_gvandra_2021.md"
-# _REPORT_NAME = "../x.md"
+_PHOTOS_DESC_NAME = "images.tsv"
+_REPORT_NAME = "../source_report_gvandra_2021.md"
+_OUTPUT_REPORT_NAME = "../report_gvandra_2021.md"
+
+
+def _read_file(file_name):
+    text = None
+    with open(file_name, encoding="utf-8") as f:
+        text = f.read()
+    return text
 
 
 def main():
-    report_text = None
-    with open(_REPORT_NAME, encoding="utf-8") as f_r:
-        report_text = f_r.read()
+    source_report_text = _read_file(_REPORT_NAME)
 
-    with open(sys.argv[1], encoding='utf-8') as f:
+    photos = {}
+
+    with open(_PHOTOS_DESC_NAME, encoding='utf-8') as f:
         first = True
         prev_day = None
 
         photo_block = ""
+        day = None
+        proper_in_day = 1
+
+        for line in f:
+            if first:
+                print('SKIPPED:', line)
+                first = False
+                continue
+
+            line = line.strip()
+            cols = [col.strip() for col in line.split('\t')]
+            if len(cols) < 5:
+                print('BAD', cols)
+
+            photo_id = cols[0]
+            day, in_day_id = photo_id.split('-')
+            image_name = cols[1].replace(' ', '')
+            # author = cols[2]
+            # todo = cols[3]
+            description = cols[4]
+            if description[-1] == '.':
+                # cut dots
+                description = description[:-1]
+
+            assert proper_in_day is not None and proper_in_day > 0 and proper_in_day < 30
+
+            photo = {
+                "day": day,
+                "in_day": proper_in_day,
+                "image_name": image_name,
+                "description": description,
+            }
+            photos[image_name] = photo
+
+            if prev_day != day:
+                # skip new line at next day
+                prev_day = day
+                proper_in_day = 1
+                print()
+
+            print('{:3}   {:3}   {:30}   {}'.format(day, proper_in_day, image_name, description))
+
+            if str(proper_in_day) != in_day_id:
+                # print("Inconsistent numbering at", photo_id)
+                # sys.exit(1)
+                pass
+
+            proper_in_day += 1
+
+    print("Photos loaded:", len(photos))
+    sys.exit(1)
+
+
+
+
+
+    with open(_PHOTOS_DESC_NAME, encoding='utf-8') as f:
+        first = True
+        prev_day = None
+        photo = {}
+
         day = None
 
         for line in f:
