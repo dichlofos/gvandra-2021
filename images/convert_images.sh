4bf29fa1f091dfb7f2892436fbd78892285f7662
Mikhail Veltishchev <mvel@yandex-team.ru>
2021-11-30 23:13:32 +0300

    Refactor script to make it less buggy

--- images/convert_images.sh
+++ images/convert_images.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-mkdir -p reduced
+dir_name="sample_1600"
+mkdir -p $dir_name
 for i in *.jpg ; do
-    convert -width 1200 $i reduced/$i
+    echo $i
+    convert -resize 1600x1200 -quality 93 $i $dir_name/$i
 done
 
--- images/preprocess.py
+++ images/preprocess.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import re
 
 
@@ -57,39 +58,7 @@ _TEST_TEXT = (
 *м.н. – д.р. Уллу-Езень - лед. Хасанкой-Сюрюлген - пер. Кичкинекол В. (Сварщиков) (1А) – д. притока р. Кичкинекол*""")
 
 
-def main():
-    replace_result = _flush_block("1", "REPLACEMENT", _TEST_TEXT)
-    assert replace_result != _TEST_TEXT
-
-    source_report_text = _read_file(_REPORT_NAME)
-    assert source_report_text
-
-    # regex test
-    assert _TEST_TEXT in source_report_text
-
-    # fix times in tables
-    source_lines = source_report_text.split('\n')
-    fixed_source_lines = []
-    for line in source_lines:
-        if '|' in line:
-            new_line = re.sub(r'([0-9]+)ч ([0-9]+)м', r'\1:\2', line)
-            if line != new_line:
-                print("Fixed times in:", new_line)
-                line = new_line
-        fixed_source_lines.append(line)
-    new_source_report_text = '\n'.join(fixed_source_lines)
-    if source_report_text != new_source_report_text:
-        # write fixed source
-        print("Source fixed, writing output to", _REPORT_NAME)
-        with open(_REPORT_NAME, 'w', encoding='utf-8') as f:
-            f.write(new_source_report_text)
-        source_report_text = new_source_report_text
-
-    report_text = source_report_text
-    # report_text =  _TEST_TEXT
-    report_text = report_text.replace('\r', '')
-    # report_text = report_text.replace('\n', '__WRAP__')
-
+def _load_photos():
     photos = {}
     photos_by_day = {}
 
@@ -97,7 +66,6 @@ def main():
         first = True
         prev_day = None
 
-        photo_block = ""
         day = None
         proper_in_day = 1
 
@@ -159,21 +127,31 @@ def main():
 
             proper_in_day += 1
 
-    print("Photos loaded:", len(photos))
+    assert len(photos) == sum([len(photos_by_day[k]) for k in photos_by_day])
 
+    print("Photos loaded:", len(photos))
+    return photos, photos_by_day
+
+
+def _replace_photo_blocks(photos_by_day, report_text):
     # first, replace photo marks by days
     for day in photos_by_day:
         day_photos = photos_by_day[day]
         photo_block = ""
         for photo in day_photos:
+            image_name = photo["image_name"]
             photo_id = "{}-{}".format(day, photo["in_day"])
+            photo_link = "sample_1600/{image_name}.jpg".format(image_name=image_name)
+            print("LINK ", photo_link)
+            assert os.path.exists(photo_link)
+
             md_line = (
                 '<a name="ph_{photo_id}"></a>\n'
-                '![](images/reduced/{image_name}.jpg "Фото {photo_id}. {description}")\n'
+                '![](images/sample_1600/{image_name}.jpg "Фото {photo_id}. {description}")\n'
                 '<p style="text-align: center;">{photo_id}. {description}</p>\n\n'
             ).format(
                 photo_id=photo_id,
-                image_name=photo["image_name"],
+                image_name=image_name,
                 description=photo["description"],
             )
             print(md_line)
@@ -182,12 +160,12 @@ def main():
         print("Replacing block in day", day)
         new_report_text = _flush_block(day, photo_block, report_text)
 
-        # print(new_report_text)
-        # print("OLD:")
-        # print(report_text)
         assert new_report_text != report_text  # check replacement is really done
         report_text = new_report_text
+        return report_text
 
+
+def _replace_photo_links(photos, report_text):
     for image_name in photos:
         photo = photos[image_name]
         day = photo["day"]
@@ -200,9 +178,49 @@ def main():
         assert new_report_text != report_text
         report_text = new_report_text
 
+    return report_text
+
+
+def main():
+    replace_result = _flush_block("1", "REPLACEMENT", _TEST_TEXT)
+    assert replace_result != _TEST_TEXT
+
+    source_report_text = _read_file(_REPORT_NAME)
+    assert source_report_text
+
+    # regex test
+    assert _TEST_TEXT in source_report_text
+
+    # fix times in tables
+    source_lines = source_report_text.split('\n')
+    fixed_source_lines = []
+    for line in source_lines:
+        if '|' in line:
+            new_line = re.sub(r'([0-9]+)ч ([0-9]+)м', r'\1:\2', line)
+            if line != new_line:
+                print("Fixed times in:", new_line)
+                line = new_line
+        fixed_source_lines.append(line)
+    new_source_report_text = '\n'.join(fixed_source_lines)
+    if source_report_text != new_source_report_text:
+        # write fixed source
+        print("Source fixed, writing output to", _REPORT_NAME)
+        with open(_REPORT_NAME, 'w', encoding='utf-8') as f:
+            f.write(new_source_report_text)
+        source_report_text = new_source_report_text
+
+    report_text = source_report_text
+    # report_text =  _TEST_TEXT
+    report_text = report_text.replace('\r', '')
+    # report_text = report_text.replace('\n', '__WRAP__')
+
+    photos, photos_by_day = _load_photos()
+    report_text = _replace_photo_blocks(photos_by_day, report_text)
+    report_text = _replace_photo_links(photos, report_text)
+
     # write output
     with open(_OUTPUT_REPORT_NAME, 'w', encoding='utf-8') as f:
-        f.write(new_report_text)
+        f.write(report_text)
 
     print("Output written to ", _OUTPUT_REPORT_NAME)
 
