cdc91dfa90697dc96ef51711a942da87ab628e20
Mikhail Veltishchev <mvel@yandex-team.ru>
2021-12-29 02:29:05 +0300

    Title page reworked completely using latex formatting.

--- generate_all.sh
+++ generate_all.sh
@@ -3,7 +3,7 @@ set -xe
 cd images
 python3 preprocess.py pandoc
 cd -
-pandoc report_gvandra_2021_ch.md \
+pandoc report_gvandra_2021_ch.md metadata.yaml \
     -V mainfont="Liberation Serif" \
     -V papersize="a4" \
     -V geometry="top=1cm" \
