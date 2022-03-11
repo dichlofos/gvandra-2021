e8664d0d25c2a57e69ff76c97fe85bb4b36b6376
Mikhail Veltishchev <mvel@yandex-team.ru>
2022-03-11 14:18:20 +0300

    Check TODOs

--- generate_all.sh
+++ generate_all.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -xe
 python3 preprocess.py pandoc
+
+pandoc report_gvandra_2021_pdf.md metadata.yaml \
+    -V mainfont="Liberation Serif" \
+    -V papersize="a4" \
+    -V geometry="top=1cm" \
+    -V geometry="left=1cm" \
+    -V geometry="right=1cm" \
+    -V geometry="bottom=2cm" \
+    --pdf-engine=xelatex \
+    -o report_gvandra_2021_pdf.pdf
+
 pandoc report_gvandra_2021_ch.md metadata.yaml \
     -V mainfont="Liberation Serif" \
     -V papersize="a4" \
@@ -11,3 +22,6 @@ pandoc report_gvandra_2021_ch.md metadata.yaml \
     --pdf-engine=xelatex \
     -o report_gvandra_2021_ch.pdf
 
+if grep TODO report_gvandra_2021.md ; then
+    echo "WARNING: Some TODOs not resolved yet"
+fi
